5860bd9855ff1bcdcac9c50cb78c52a58735f8db
Falderian <nikitastaselovich@gmail.com>
2022-05-21 21:34:27 +0300

    feat: solved 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+   if(err) throw err;
+   files.forEach(item => {        
+        fs.stat(path.join(__dirname, 'secret-folder', item), (error, stat) => {
+            if(stat.isDirectory(item)) return;
+            console.log(path.parse(item).name + ' ' + '-' + ' ' + path.parse(item).ext.slice(1,4) + ' ' + '-' + ' ' + (stat.size*0.001)+'kb');
+        });            
+   })
+});
