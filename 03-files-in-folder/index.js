d218c0a1b575bd9f57505f1db947e81f3b616724
Falderian <nikitastaselovich@gmail.com>
2022-05-24 21:50:13 +0300

    fix: fixed 06 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
    files.forEach(item => {        
         fs.stat(path.join(__dirname, 'secret-folder', item), (error, stat) => {
             if(stat.isDirectory(item)) return;
-            console.log(path.parse(item).name + ' ' + '-' + ' ' + path.parse(item).ext.slice(1,4) + ' ' + '-' + ' ' + (stat.size*0.001)+'kb');
+            console.log(path.parse(item).name + ' ' + '-' + ' ' + path.parse(item).ext.slice(1) + ' ' + '-' + ' ' + (stat.size*0.001)+'kb');
         });            
    })
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,8 +5,17 @@ const pathToFilesFolder = path.join(__dirname, 'files');
 const pathToFilesCopyFolder = path.join(__dirname, 'files-copy');
 
 function copyDir() {    
+    
+    fs.readdir(pathToFilesCopyFolder , (err, files) => {
+        if(err) throw err;
+        files.forEach(item => {
+            fs.unlink(path.join(pathToFilesCopyFolder,item) , (err) => {
+                if (err) {console.log()};
+            })
+        });
+    })
+     
     fs.mkdir(pathToFilesCopyFolder, { recursive: true }, err => {if(err) return;});
-
     fs.readdir(pathToFilesFolder , (err, files) => {
         if(err) throw err;
         files.forEach(item => {        
